591f206115ab75e8df79a7de7cedc98370b24f61
minsoo <minsoo651@naver.com>
2023-03-05 19:17:39 +0900

    fix: 스키자 자동생성하도록 수정

---
+++ src/main/java/org/example/Team.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+
+
+    @Column
+    private String name;
+
+
+}
